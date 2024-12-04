540ec9a3078463ed0f0bd4b50fa5663a1757aa9d
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-04 22:25:06 +0300

    Enums was Created

---
+++ AIDreamDecoder.Domain/Enums/AnalysisStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Domain.Enums
+{
+    public enum AnalysisStatus
+    {
+        Pending = 0, // Analiz bekleniyor
+        InProgress = 1, // Analiz devam ediyor
+        Completed = 2, // Analiz tamamlandı
+        Failed = 3 // Analiz başarısız
+    }
+}
