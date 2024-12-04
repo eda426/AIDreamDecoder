eadeeb4352ebcacab961e28af16d5633909f3537
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-04 16:36:36 +0300

    OpenAiApiException Created

---
+++ AIDreamDecoder/Exceptions/OpenAiApiException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Exceptions
+{
+    public class OpenAiApiException : Exception
+    {
+        public int StatusCode { get; }
+
+        public OpenAiApiException(string message, int statusCode)
+            : base(message)
+        {
+            StatusCode = statusCode;
+        }
+
+        public OpenAiApiException(string message, int statusCode, Exception innerException)
+            : base(message, innerException)
+        {
+            StatusCode = statusCode;
+        }
+
+        public override string ToString()
+        {
+            return $"OpenAI API Error: {Message} (Status Code: {StatusCode})";
+        }
+    }
+}
