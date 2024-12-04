1f88cdec265ceb077e04e61f7c2b772c1934f2bb
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-04 22:26:08 +0300

    ValueObjects Created

---
+++ AIDreamDecoder.Domain/ValueObjects/AnalysisResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Domain.ValueObject
+{
+    public class AnalysisResult
+    {
+        public string Summary { get; set; } // Rüyanın özeti
+        public string EmotionalTone { get; set; } // Rüyanın duygusal tonu
+        public List<string> KeySymbols { get; set; } // Rüya içindeki önemli semboller
+
+        public AnalysisResult()
+        {
+            KeySymbols = new List<string>();
+        }
+    }
+}
