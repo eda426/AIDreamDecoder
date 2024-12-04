f34a41a1803b78bf9c2ed8767e3566d957bf1101
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-04 16:19:55 +0300

    Interfaces Created

---
+++ AIDreamDecoder/Interfaces/IHistoryService.cs
@@ -0,0 +1,38 @@
+﻿using AIDreamDecoder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Interfaces
+{
+    public interface IHistoryService
+    {
+        /// <summary>
+        /// Kullanıcının tüm geçmiş rüya analizlerini getirir.
+        /// </summary>
+        /// <param name="userId">Kullanıcının kimliği.</param>
+        /// <returns>Kullanıcının rüya analiz geçmişi.</returns>
+        Task<IEnumerable<AnalysisResult>> GetDreamAnalysisHistoryAsync(int userId);
+
+        /// <summary>
+        /// Belirli bir rüya için analiz geçmişini getirir.
+        /// </summary>
+        /// <param name="dreamId">Rüya kimliği.</param>
+        /// <returns>Rüya analiz sonucu.</returns>
+        Task<AnalysisResult> GetAnalysisResultByDreamIdAsync(int dreamId);
+
+        /// <summary>
+        /// Kullanıcının belirli bir tarih aralığındaki analizlerini getirir.
+        /// </summary>
+        /// <param name="userId">Kullanıcının kimliği.</param>
+        /// <param name="startDate">Başlangıç tarihi.</param>
+        /// <param name="endDate">Bitiş tarihi.</param>
+        /// <returns>Tarih aralığındaki analizler.</returns>
+        Task<IEnumerable<AnalysisResult>> GetDreamAnalysisHistoryByDateRangeAsync(int userId, DateTime startDate, DateTime endDate);
+
+        //Kullanıcıların geçmiş rüya analizlerini listelemeyi ve tarih aralığı gibi özel filtreleme seçenekleri sunar.
+        //GetDreamAnalysisHistoryByDateRangeAsync ile belirli bir zaman dilimine yönelik analizler alınabilir.
+    }
+}
---
+++ AIDreamDecoder/Interfaces/IUserService.cs
@@ -0,0 +1,46 @@
+﻿using AIDreamDecoder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Interfaces
+{
+    public interface IUserService
+    {
+        /// <summary>
+        /// Yeni bir kullanıcı oluşturur.
+        /// </summary>
+        /// <param name="firstName">Kullanıcının adı.</param>
+        /// <param name="lastName">Kullanıcının soyadı.</param>
+        /// <param name="email">Kullanıcının e-posta adresi.</param>
+        /// <param name="password">Kullanıcının şifresi.</param>
+        /// <returns>Oluşturulan kullanıcı.</returns>
+        Task<User> RegisterUserAsync(string firstName, string lastName, string email, string password);
+
+        /// <summary>
+        /// Kullanıcının giriş yapmasını sağlar.
+        /// </summary>
+        /// <param name="email">Kullanıcının e-posta adresi.</param>
+        /// <param name="password">Kullanıcının şifresi.</param>
+        /// <returns>Kullanıcı bilgisi.</returns>
+        Task<User> AuthenticateUserAsync(string email, string password);
+
+        /// <summary>
+        /// Kullanıcının kimlik bilgilerine göre verilerini alır.
+        /// </summary>
+        /// <param name="userId">Kullanıcının kimliği.</param>
+        /// <returns>Kullanıcı bilgisi.</returns>
+        Task<User> GetUserByIdAsync(int userId);
+
+        /// <summary>
+        /// Kullanıcıyı sistemden siler.
+        /// </summary>
+        /// <param name="userId">Kullanıcının kimliği.</param>
+        Task DeleteUserAsync(int userId);
+
+        //Kullanıcı oluşturma, giriş yapma, bilgileri alma ve silme gibi temel yönetim işlemlerini kapsar.
+       //RegisterUserAsync ve AuthenticateUserAsync ile kullanıcı işlemleri güvenli hale getirilir.
+    }
+}
