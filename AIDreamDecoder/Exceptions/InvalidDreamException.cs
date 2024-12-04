2477ac7013a9207ad4512830de1ee706d7bb2aac
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-04 16:37:27 +0300

    InvalidDreamException Created

---
+++ AIDreamDecoder/Exceptions/InvalidDreamException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Exceptions
+{
+    public class InvalidDreamException : Exception
+    {
+        public InvalidDreamException(string message)
+            : base(message)
+        {
+        }
+
+        public InvalidDreamException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public override string ToString()
+        {
+            return $"Invalid Dream Error: {Message}";
+        }
+    }
+}
