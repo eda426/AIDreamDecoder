9fb507091f22acd1cb050cf4a6a2c04e4bbcf743
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-07 22:54:11 +0300

    CreateDreamRequestDto Created

---
+++ AIDreamDecoder.Application/Dtos/DreamDtos/CreateDreamRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Application.Dtos.DreamDtos
+{
+    public class CreateDreamRequestDto
+    {
+        //Kullanıcının yeni bir rüya kaydı göndermesi için gerekli olan bilgileri içerir.
+        public string DreamDescription { get; set; } // Rüyanın açıklaması
+        public Guid UserId { get; set; } // Kullanıcının benzersiz kimliği
+    }
+}
