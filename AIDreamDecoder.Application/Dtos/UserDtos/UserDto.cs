8ac157892011ed2f01159d8c66e05e19986281e7
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-07 23:03:06 +0300

    UserDto Created

---
+++ AIDreamDecoder.Application/Dtos/UserDtos/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Application.Dtos.UserDtos
+{
+    public class UserDto
+    {
+        //Kullanıcı bilgilerini taşımak için kullanılır.
+        public Guid Id { get; set; } // Kullanıcı kimliği
+        public string Name { get; set; } // Kullanıcı adı
+        public string Email { get; set; } // Kullanıcı emaili
+    }
+}
