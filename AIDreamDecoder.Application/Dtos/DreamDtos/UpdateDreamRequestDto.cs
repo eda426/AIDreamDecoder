3bb0c2dc15e6a51b1227a7c830470920eaa71487
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-07 22:59:03 +0300

    UpdateDreamRequestDto Created

---
+++ AIDreamDecoder.Application/Dtos/DreamDtos/UpdateDreamRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Application.Dtos.DreamDtos
+{
+    public class UpdateDreamRequestDto
+    {
+        //Var olan bir rüyayı güncellemek için kullanılır.
+        public int DreamId { get; set; } // Güncellenecek rüya kimliği
+        public string UpdatedDescription { get; set; } // Yeni rüya açıklaması
+    }
+}
