ce03126226e2d2f2d8c16d96326ea0135834e9bc
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-07 23:03:51 +0300

    UserRegistrationDto Created

---
+++ AIDreamDecoder.Application/Dtos/UserDtos/UserRegistrationDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Application.Dtos.UserDtos
+{
+    public class UserRegistrationDto
+    {
+        //Kullanıcı kayıt işlemi için gerekli verileri içerir.
+        public string Name { get; set; } // Kullanıcı adı
+        public string Email { get; set; } // Kullanıcı emaili
+        public string Password { get; set; } // Şifre
+    }
+}
