94a0bb9cdc8b4d18a4f5e784ccbd2cf9b3dbe8e3
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-07 23:01:42 +0300

    DreamAnalysisDto Ccreated

---
+++ AIDreamDecoder.Application/Dtos/DreamAnalysisDtos/DreamAnalysisDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Application.Dtos.DreamAnalysisDtos
+{
+    public class DreamAnalysisDto
+    {
+        //Rüya analizlerinin detaylarını göstermek için kullanılır.
+        public int Id { get; set; } // Analiz kimliği
+        public int DreamId { get; set; } // Analizi yapılan rüyanın kimliği
+        public string AnalysisResult { get; set; } // Analiz sonucu (yorum, anlam vs.)
+        public string Status { get; set; } // Analiz durumu (Tamamlandı, İşleniyor vs.)
+        public DateTime CreatedAt { get; set; } // Analizin oluşturulma tarihi
+    }
+}
