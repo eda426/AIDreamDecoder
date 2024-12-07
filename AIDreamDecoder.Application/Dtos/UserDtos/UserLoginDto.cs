6156153a93a686db19d802a017677bf007b8a9a8
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-07 23:04:37 +0300

    UserLoginDto Created

---
+++ AIDreamDecoder.Application/Dtos/UserDtos/UserLoginDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Application.Dtos.UserDtos
+{
+    public class UserLoginDto
+    {
+        // Kullanıcının giriş yapması için gerekli bilgileri içerir
+        public string Email { get; set; } // Kullanıcı emaili
+        public string Password { get; set; } // Şifre
+    }
+}
