c69a1ce4f56e2ff5567f093960df9c5a1830b2de
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-30 23:27:03 +0300

    IDreamRepository Created

---
+++ AIDreamDecoder.Infrastructure/Repositories/IDreamRepository.cs
@@ -0,0 +1,18 @@
+﻿using AIDreamDecoder.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Infrastructure.Repositories
+{
+    public interface IDreamRepository
+    {
+        Task<List<Dream>> GetAllDreamsAsync(); // Tüm rüyaları getir
+        Task<Dream> GetDreamByIdAsync(Guid id); // Belirli bir rüyayı getir
+        Task AddDreamAsync(Dream dream); // Yeni rüya ekle
+        Task<bool> DeleteDreamAsync(Guid id); // Rüya sil
+    }
+
+}
