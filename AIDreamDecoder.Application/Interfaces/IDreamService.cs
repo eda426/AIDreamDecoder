e471f9a7ce664cba0002a6132d2ddd93295998cf
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-30 23:03:26 +0300

    DreamService Created Application

---
+++ AIDreamDecoder.Application/Interfaces/IDreamService.cs
@@ -0,0 +1,18 @@
+﻿using AIDreamDecoder.Application.Dtos.DreamDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIDreamDecoder.Application.Interfaces
+{
+    public interface IDreamService
+    {
+        Task<List<DreamDto>> GetDreamsAsync(); // Tüm rüyaları getir
+        Task<DreamDto> GetDreamByIdAsync(Guid id); // Belirli bir rüyayı getir
+        Task<Guid> AddDreamAsync(DreamDto dreamDto); // Yeni rüya ekle
+        Task<bool> DeleteDreamAsync(Guid id); // Rüya sil
+    }
+
+}
