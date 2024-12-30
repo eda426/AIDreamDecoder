169bc7a642cfb2f41b815661a786f04177db1b37
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-12-31 00:53:53 +0300

    Testing a protected endpoint to test the JWT token

---
+++ AIDreamDecoder.WebAPI/Controllers/ProtectedController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AIDreamDecoder.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProtectedController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetProtectedData()
+        {
+            return Ok("Bu veri yalnızca yetkili kullanıcılar içindir.");
+        }
+    }
+}
+//JWT token doğrulamasını test etmek için bir korunan endpoint 
