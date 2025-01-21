ae4b10c9dcf8196034509c943492bb76921d24b3
Ftm-btl <88613684+Ftm-btl@users.noreply.github.com>
2025-01-21 23:20:12 +0300

    Update DreamDto

--- AIDreamDecoder.Application/Dtos/DreamDtos/DreamDto.cs
+++ AIDreamDecoder.Application/Dtos/DreamDtos/DreamDto.cs
@@ -1,6 +1,7 @@
 ﻿using AIDreamDecoder.Domain.Entities;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace AIDreamDecoder.Application.Dtos.DreamDtos
 {
     public class DreamDto
     { // Rüya detaylarını listelemek veya göstermek için kullanılır.
+        [Required]
         public Guid Id { get; set; } // Rüya kimliği
         public string Description { get; set; } // Rüyanın metin açıklaması
         public DateTime CreatedAt { get; set; } // Rüyanın oluşturulma tarihi
